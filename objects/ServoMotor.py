8879dea8a86e9b7f891b7f441d4c8c1b52c13be2
Justin Shaytar <jas7650@rit.edu>
2024-02-26 15:34:43 -0500

    Code to try on servo

--- objects/ServoMotor.py
+++ objects/ServoMotor.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as IO          #calling header file which helps us use GPIO’s of PI
+# import RPi.GPIO as IO          #calling header file which helps us use GPIO’s of PI
 import time                            #calling time to provide delays in program
 
 # Period -> 0.00333333333 seconds -> 3330 microseconds
@@ -6,20 +6,28 @@ import time                            #calling time to provide delays in progra
 # Pulses range from 500-2500 us
 # 1500 us is centered
 # 1000-2000 us is counterclockwise
-FREQUENCY = 400
-PERIOD = 1/FREQUENCY
-print(PERIOD)
-MAX_LEFT = 500/PERIOD
-MAX_RIGHT = 2500
+PERIOD_US = 5000   # 5000 us
+PERIOD = PERIOD_US * pow(10, -6)
+print(f"Period: {PERIOD_US} us")
+FREQUENCY_MHZ = 1/PERIOD
+FREQUENCY = 1/PERIOD_US
+print(f"Frequency: {FREQUENCY_MHZ} Mhz, {FREQUENCY} Hz")
+MIN_DUTY = round(500/PERIOD_US * 100, 2)
+MAX_DUTY = round(2500/PERIOD_US * 100, 2)
+MIN_PULSE_TIME = round((PERIOD_US*MIN_DUTY)/100, 2)
+MAX_PULSE_TIME = round((PERIOD_US*MAX_DUTY)/100, 2)
+print(f"Min Duty Cycle: {MIN_DUTY}%, Min Pulse Time: {MIN_PULSE_TIME} us")
+print(f"Max Duty Cycle: {MAX_DUTY}%, Max Pulse Time: {MAX_PULSE_TIME}us")
+
 
 
 IO.setwarnings(False)
 IO.setmode (IO.BCM)
 IO.setup(12,IO.OUT)
-p = IO.PWM(12,300)
+p = IO.PWM(12,FREQUENCY)
 p.start(0)
 while 1:
     p.ChangeDutyCycle(MAX_LEFT)
-    time.sleep(10)
-    p.ChangeDutyCycle(MAX_RIGHT)
-    time.sleep(10)
+    # time.sleep(10)
+    # p.ChangeDutyCycle(MAX_RIGHT)
+    # time.sleep(10)
