cb4c07ebf9708cff1c35acd293b3b775fffb5292
Justin Shaytar <jas7650@rit.edu>
2024-02-26 16:09:29 -0500

    Code to try on servo

--- objects/ServoMotor.py
+++ objects/ServoMotor.py
@@ -28,6 +28,46 @@ p = IO.PWM(12,FREQUENCY_MHZ)
 p.start(0)
 while 1:
     p.ChangeDutyCycle(MAX_DUTY)
-    time.sleep(10)
+    print("At max")
+    time.sleep(3)
     p.ChangeDutyCycle(MIN_DUTY)
-    time.sleep(10)
+    print("At min")
+    time.sleep(3)
+
+
+
+# import RPi.GPIO as IO          #calling header file which helps us use GPIO’s of PI
+# import time                            #calling time to provide delays in program
+
+# Period -> 0.00333333333 seconds -> 3330 microseconds
+
+# Pulses range from 500-2500 us
+# 1500 us is centered
+# 1000-2000 us is counterclockwise
+
+
+# class ServoMotor(object):
+#     def __init__(self, period_us):
+#         period_us = 5000   # 5000 us
+#         period = period_us * pow(10, -6)
+#         frequency_mhz = 1/period
+#         frequency = 1/period_us
+#         min_duty = round(500/period_us * 100, 2)
+#         max_duty = round(2500/period_us * 100, 2)
+#         IO.setwarnings(False)
+#         IO.setmode (IO.BCM)
+#         IO.setup(12,IO.OUT)
+#         p = IO.PWM(12,frequency_mhz)
+#         p.start(0)
+
+
+#     def drop_ball():
+
+
+# p = IO.PWM(12,frequency_mhz)
+# p.start(0)
+# while 1:
+#     p.ChangeDutyCycle(MAX_DUTY)
+#     time.sleep(10)
+#     p.ChangeDutyCycle(MIN_DUTY)
+#     time.sleep(10)
